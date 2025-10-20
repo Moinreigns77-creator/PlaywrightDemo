d0cd0342d8dacbc2b1d2d9b0e3d6f9cc6602bfdb
Moinreigns77-creator <shaikmoin1399@gmail.com>
2025-10-20 23:01:29 +0530

    new commit 20/10/2025

--- tests/sample2.spec.js
+++ tests/sample2.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { link } from "fs";
 
 // test.use({viewport:{width:1920,height:1080}});
 
@@ -186,6 +187,247 @@ test("Keyboard Actions", async ({ page }) => {
 
     await page.waitForTimeout(3000);
 
-       await page.keyboard.press("Enter");
+    await page.keyboard.press("Enter");
 })
 
+test("Auto Suggestions handling", async ({ page }) => {
+    await page.goto("https://www.google.com/");
+    await page.locator("//textarea[@name='q']").fill("Playwright");
+
+    await page.waitForSelector("//li[@role='presentation']");
+
+    const elements = await page.$$("//li[@role='presentation']");
+
+    for (let i of elements) {
+        const value = await i.textContent();
+
+        if (value.includes("mcp server")) {
+            await i.click();
+            break;
+        }
+    }
+
+    await page.waitForTimeout(3000);
+})
+
+test("Drag and Drop", async ({ page }) => {
+    await page.goto("https://testautomationpractice.blogspot.com/#");
+
+    await page.dragAndDrop("#draggable", "#droppable");
+
+    await page.waitForTimeout(5000);
+})
+
+test("Alerts Handling", async ({ page }) => {
+
+    await page.goto("https://testautomationpractice.blogspot.com/#");
+
+    await page.on("dialog", async (dialogWin) => {
+        var type = await dialogWin.type();
+
+        console.log(`Alert Type: ${type}`);
+
+        const msg = await dialogWin.message();
+        console.log(`Message :${msg}`);
+
+        await dialogWin.accept();
+
+    })
+
+    await page.locator("#alertBtn").click();
+
+    // await page.waitForTimeout(3000);
+})
+
+test("Confirm Alert", async ({ page }) => {
+    await page.goto("https://testautomationpractice.blogspot.com/#");
+
+    //Confirm alert
+
+    page.on("dialog", async (dialogWin2) => {
+        var type = dialogWin2.type();
+        console.log(`Alert type: ${type}`);
+
+        var message = dialogWin2.message();
+        console.log(`Message : ${message}`);
+
+        dialogWin2.dismiss();
+
+    })
+
+    await page.locator("#confirmBtn").click();
+})
+
+test("Prompt Alert", async ({ page }) => {
+    await page.goto("https://testautomationpractice.blogspot.com/#");
+
+    page.on("dialog", (dialogwin3) => {
+        const type = dialogwin3.type();
+        console.log(`Type : ${type}`);
+
+        const message = dialogwin3.message();
+        console.log(`Message: ${message}`);
+
+        dialogwin3.accept("Mohd Moin");
+
+    })
+
+    await page.locator("#promptBtn").click();
+
+    const status = await page.locator("#demo");
+    await expect(status).toContainText("Mohd Moin");
+
+    const status2 = await page.locator("#demo").textContent();
+    await expect(status2).toContain("Mohd Moin");
+
+    const status3 = await page.locator("#demo");
+    await expect(status3).toHaveText("Hello Mohd Moin! How are you today?")
+
+})
+
+
+test("Frames handling", async ({ page }) => {
+    await page.goto("https://ui.vision/demo/webtest/frames/");
+
+    const frame_1 = await page.frameLocator("//frame[@src='frame_1.html']");
+
+    await frame_1.locator("input[name='mytext1']").fill("Moin");
+
+    // await frame_1.fill("input[name='mytext1']","Mohd");
+    // await page.waitForTimeout(2000);
+
+    const frame_2 = await page.frameLocator("//frame[@src='frame_2.html']");
+
+    await frame_2.locator("input[name='mytext2']").fill("Moin");
+
+    // await page.waitForTimeout(2000);
+
+    await frame_1.locator("input[name='mytext1']").focus();
+    const frame_3 = await page.frameLocator("//frame[@src='frame_3.html']");
+
+    const nestedFrame = await frame_3.frameLocator("//iframe[contains(@src,'google.com')]");
+
+    await nestedFrame.locator("//span[text()='I am a human']").click();
+
+    // await page.waitForTimeout(2000);
+
+    await page.frameLocator("//frame[@src='frame_4.html']").locator("input[name='mytext4']").fill("Mohammad");
+
+    await page.waitForTimeout(2000);
+
+})
+
+test("Handling iFrames using frame() function", async ({ page }) => {
+    await page.goto("https://ui.vision/demo/webtest/frames/");
+
+    const frame1 = await page.frame({ url: /.*frame_1.*/ });
+    // const frame2 = await page.frame({name:"frame1"});
+
+    await frame1.fill("input[name='mytext1']", "Mohd");
+
+    // await frame2.fill("input[name='mytext1']","ABCD");
+
+    await page.keyboard.press("Control+A");
+
+    await page.waitForTimeout(3000);
+})
+
+test("Multiple tabs", async ({ browser }) => {
+    const context = await browser.newContext();
+
+    const mainPage = await context.newPage();
+
+    await mainPage.goto("https://freelance-learn-automation.vercel.app/signup");
+
+    const [twitterPage] = await Promise.all(
+        [
+            context.waitForEvent("page"),
+
+            mainPage.locator("//a[contains(@href,'twitter')]").click()
+        ]
+    )
+
+    await twitterPage.locator("//a[@href='/login']").click();
+    await twitterPage.locator("//input[@autocomplete='username']").fill("avdcs");
+    await twitterPage.waitForTimeout(3000);
+
+    await mainPage.bringToFront();
+
+    await mainPage.waitForTimeout(2000);
+
+    const [facebookPage] = await Promise.all(
+        [
+            context.waitForEvent("page"),
+            mainPage.locator("//a[contains(@href,'facebook')]").click()
+        ]
+    )
+
+    await facebookPage.waitForTimeout(2000);
+
+    await facebookPage.locator("//span[normalize-space()='Email address or phone number']/following-sibling::input[@name='email']").fill("shaikmoin2060@gmail.com");
+
+    await facebookPage.locator("//span[normalize-space()='Password']/following-sibling::input[@name='pass']").fill("76326336@M");
+
+    await facebookPage.locator("(//div[@aria-label='Log in to Facebook' and @role='button']//span[normalize-space()='Log in'])[1]").click();
+
+    await facebookPage.waitForTimeout(2000);
+})
+
+
+test("Network call", async ({ page }) => {
+    await page.goto("https://www.facebook.com");
+
+    await page.locator("//a[@href='/r.php?entry_point=login']").click();
+
+    await page.waitForLoadState("networkidle");
+
+    const inputs = await page.locator("//input[@type='text']").count();
+
+    await expect(inputs).toBe(6);
+
+})
+
+import loginData  from "../JsonFiles/loginData.json" assert {type:"json"}
+
+test("Register using data from JSON", async ({ page }) => {
+    await page.goto("https://freelance-learn-automation.vercel.app/login");
+
+    await page.locator("#email1").fill(loginData.email);
+
+    await page.locator("#password1").fill(loginData.password);
+
+    // await page.pause();
+
+    await page.getByRole("button", { name: "Sign in" }).click();
+
+    await page.waitForTimeout(3000);
+
+    await expect(page.locator(".cartBtn")).toHaveRole("button");
+    await expect(page.locator(".cartBtn")).toBeVisible();
+})
+
+
+import loginData1 from  "../JsonFiles/loginDataMultipleUsers.json" assert {type:'json'}
+
+test.describe("Login into App using multiple users Data driven testing",()=>{
+
+for(const data of loginData1){
+
+    test.describe(`Login with user: ${data.id}`, ()=>{
+
+        test("Login to App", async({page})=>{
+            await page.goto("https://freelance-learn-automation.vercel.app/login");
+
+            await page.locator("#email1").fill(data.email);
+
+            await page.locator("input[name='password1']").fill(data.password);
+
+            await page.getByRole("button",{name:"Sign in"}).click();
+
+          await  expect(page.locator(".cartBtn")).toBeVisible();
+
+        })
+    })
+}
+
+})
